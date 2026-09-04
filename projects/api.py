758cad09da82596f7a9a5413c92487a4af0cac0b
luisOrtga <ortegaluiskb@gmail.com>
2026-09-03 23:48:17 -0600

    Upgrade project api restful

---
+++ projects/api.py
@@ -0,0 +1,10 @@
+from rest_framework import permissions, viewsets
+
+from .models import Project
+from .serializers import ProjectSerializer
+
+
+class ProjectViewSet(viewsets.ModelViewSet):
+    queryset = Project.objects.all()
+    permission_classes = [permissions.AllowAny]
+    serializer_class = ProjectSerializer
